22bb5c762afb3f0145df6f94b58a3753db9bc151
LionZero01 <jonathan@liono.me>
2022-03-23 10:46:17 -0500

    Jonathan Liono

--- app/src/main/java/com/example/idea6/MainActivity.kt
+++ app/src/main/java/com/example/idea6/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.idea6
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//Jonathan Liono test
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
