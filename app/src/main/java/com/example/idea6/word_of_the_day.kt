cc62dc6e7f461f139d8d9ab4657aeb91bce45131
Hoa Truong <hoa.truongminh2000@gmail.com>
2022-04-04 17:32:27 -0500

    change the kotlin file

--- app/src/main/java/com/example/idea6/word_of_the_day.kt
+++ app/src/main/java/com/example/idea6/word_of_the_day.kt
@@ -1,15 +1,40 @@
 package com.example.idea6
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import com.example.idea6.databinding.FragmentCustomDictBinding
 
 class word_of_the_day : Fragment(R.layout.fragment_word_of_the_day) {
+
+    private var _binding: FragmentCustomDictBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCustomDictBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         
 
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
