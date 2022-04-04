3c347edafba017f46933c8200e87314822da74f9
LionZero01 <jonathan@liono.me>
2022-04-04 14:18:18 -0500

    Fixed dict Fragment, Added Fragment to main activity.

--- app/src/main/java/com/example/idea6/MainActivity.kt
+++ app/src/main/java/com/example/idea6/MainActivity.kt
@@ -2,12 +2,27 @@ package com.example.idea6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.idea6.databinding.ActivityMainBinding
 
-//Jonathan Liono test
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Get the navigation host fragment from this Activity
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        // Instantiate the navController using the NavHostFragment
+        navController = navHostFragment.navController
+        // Make sure actions in the ActionBar get propagated to the NavController
+        setupActionBarWithNavController(navController)
 
     }
 }
