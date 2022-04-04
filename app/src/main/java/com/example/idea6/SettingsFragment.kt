3ebbd164fe8be10ee272d79db91d56c994b9e996
Jihyun Eom <jihyuni46@gmail.com>
2022-04-04 11:03:58 -0500

    Additional Navigation Support, implemented drop-down list in SettingsFragment

--- app/src/main/java/com/example/idea6/SettingsFragment.kt
+++ app/src/main/java/com/example/idea6/SettingsFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.idea6.databinding.FragmentSettingsBinding
@@ -28,6 +30,35 @@ class SettingsFragment : Fragment() {
     ): View? {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        // Setup spinners for theme and text size
+        val themeSpinner: Spinner = binding.themeSpinner
+        val textSizeSpinner: Spinner = binding.textSizeSpinner
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        context?.let {
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.themes_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                themeSpinner.adapter = adapter
+            }
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.textSize_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                textSizeSpinner.adapter = adapter
+            }
+        }
+
         return view
     }
 
