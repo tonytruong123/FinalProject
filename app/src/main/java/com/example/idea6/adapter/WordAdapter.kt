4dae9e6201c5526a180f0b1845e046603d5fca1e
LionZero01 <jonathan@liono.me>
2022-04-04 09:09:01 -0500

    Added adapter and layout for custom dictionary

---
+++ app/src/main/java/com/example/idea6/adapter/WordAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.prototype1.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.idea6.R
+
+
+class WordAdapter (
+    private val context: Context,
+    //Word Class Placeholder for testing
+    private val dataset: List<String>
+
+): RecyclerView.Adapter<WordAdapter.WordViewHolder>(){
+
+    class WordViewHolder(view: View?):RecyclerView.ViewHolder(view!!){
+        val word_mainView: TextView = view!!.findViewById(R.id.main_word)
+        val word_pronView: TextView = view!!.findViewById(R.id.word_pron)
+        val word_defView: TextView = view!!.findViewById(R.id.word_def)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.word_vertical, parent, false)
+        return WordViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        holder.word_mainView.text = dataset[position]
+    }
+
+    override fun getItemCount() = dataset.size
+
+}
