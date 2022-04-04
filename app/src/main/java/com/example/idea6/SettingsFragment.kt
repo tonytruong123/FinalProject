f4d29512788d714837c03bcede49e040dc3e1a5a
Jihyun Eom <jihyuni46@gmail.com>
2022-04-04 15:02:58 -0500

    Replaced nav_graph with my_nav, additional options added & implemented to SettingsFragment

--- app/src/main/java/com/example/idea6/SettingsFragment.kt
+++ app/src/main/java/com/example/idea6/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.idea6
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -23,6 +25,11 @@ class SettingsFragment : Fragment() {
     // Allow access to SettingsViewModel
     private val SettingsViewModel: SettingsViewModel by activityViewModels()
 
+    // TimePickerDialog code provided by GeeksForGeeks; will be edited later for optimization
+    private val timePickerDialogListener: TimePickerDialog.OnTimeSetListener =
+        TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute -> // logic to properly handle
+            // the picked timings by user
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,9 +39,12 @@ class SettingsFragment : Fragment() {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        // Setup spinners for theme and text size
+        // Setup spinners
         val themeSpinner: Spinner = binding.themeSpinner
         val textSizeSpinner: Spinner = binding.textSizeSpinner
+        val displayLimitSpinner: Spinner = binding.displayLimitSpinner
+
+        val timeSelectButton: Button = binding.timeSelectButton
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         context?.let {
@@ -53,10 +63,26 @@ class SettingsFragment : Fragment() {
                 R.array.textSize_array,
                 android.R.layout.simple_spinner_item
             ).also { adapter ->
-                // Specify the layout to use when the list of choices appears
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                // Apply the adapter to the spinner
                 textSizeSpinner.adapter = adapter
+            }
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.displayLimit_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                displayLimitSpinner.adapter = adapter
+            }
+            val timePicker = TimePickerDialog(
+                it,
+                timePickerDialogListener,
+                12,
+                10,
+                false
+            )
+            timeSelectButton.setOnClickListener {
+                timePicker.show()
             }
         }
 
@@ -65,7 +91,11 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding?.apply {  }
+        binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            settingsViewModel = SettingsViewModel
+            settingsFragment = this@SettingsFragment
+        }
     }
 
     /**
