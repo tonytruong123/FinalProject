13cb1c2215c8f0fbbdbef072dc3d7b8f0bcf4f26
LionZero01 <jonathan@liono.me>
2022-04-19 22:35:29 -0500

    Added work manager (15 min interval)

--- app/src/main/java/com/example/idea6/MainActivity.kt
+++ app/src/main/java/com/example/idea6/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.idea6
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.d
@@ -12,20 +15,26 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.preference.PreferenceManager
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.work.*
 import com.example.idea6.customdict.CustomDict
 import com.example.idea6.customdict.CustomDictRoomDatabase
 import com.example.idea6.customdict.CustomDictViewModel
 import com.example.idea6.customdict.CustomDictViewModelFactory
 import com.example.idea6.databinding.ActivityMainBinding
+import com.example.idea6.worker.RefreshWorkWorker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import java.io.File
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private val customDictViewModel: CustomDictViewModel by viewModels {
         CustomDictViewModelFactory((application as CustomDictApplication).repository)
+    }
+    private val workManager by lazy {
+        WorkManager.getInstance(applicationContext)
     }
 
     private var currentTheme = LIGHT
@@ -35,6 +44,7 @@ class MainActivity : AppCompatActivity() {
         private const val LIGHT = R.style.AppTheme_Light
         private const val DARK = R.style.AppTheme_Dark
         private const val HIGH_CONTRAST = R.style.AppTheme_HighContrast
+        const val CHANNEL_ID = "new_word_id"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,11 +69,29 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
         // Make sure actions in the ActionBar get propagated to the NavController
         //setupActionBarWithNavController(navController)
-
+        createNotificationChannel()
+        createPeriodicWorkRequest()
     }
 
     public fun getDatabase(): CustomDictViewModel {
         return customDictViewModel
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     private fun getThemeFromSettings() {
@@ -72,6 +100,31 @@ class MainActivity : AppCompatActivity() {
             "Dark" -> currentTheme = DARK
             "High Contrast" -> currentTheme = HIGH_CONTRAST
         }
+    }
+
+    private fun createOneTimeWorkRequest() {
+        // 1
+        val imageWorker = OneTimeWorkRequestBuilder<RefreshWorkWorker>()
+            .addTag("Refresh Word Save")
+            .build()
+        // 2
+        workManager.enqueueUniqueWork(
+            "oneTimeRefreshSave",
+            ExistingWorkPolicy.KEEP,
+            imageWorker
+        )
+    }
+
+    private fun createPeriodicWorkRequest(){
+        //15 Min Repeating
+        val imageWorker = PeriodicWorkRequestBuilder<RefreshWorkWorker>(15, TimeUnit.MINUTES)
+            .addTag("Refresh Word Save")
+            .build()
+        workManager.enqueueUniquePeriodicWork(
+            "periodicRefeshSave",
+            ExistingPeriodicWorkPolicy.KEEP,
+            imageWorker
+        )
     }
 
     private fun setTheme() {
@@ -85,5 +138,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
 
 }
