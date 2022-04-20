1c81f43e1087be76280d76a9f23b09beb040ba94
Jihyun Eom <jihyuni46@gmail.com>
2022-04-19 23:24:22 -0500

    Remade Settings Fragment; Currently implemented themes for overall app.

---
+++ app/src/main/java/com/example/idea6/Notification.kt
@@ -0,0 +1,26 @@
+package com.example.idea6
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+
+const val notificationID = 1
+const val channelID = "channel1"
+const val titleExtra = "titleExtra"
+const val messageExtra = "messageExtra"
+
+class Notification : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val notification: Notification = NotificationCompat.Builder(context!!, channelID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(intent?.getStringExtra(titleExtra))
+            .setContentText(intent?.getStringExtra(messageExtra))
+            .build()
+
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(notificationID, notification)
+    }
+}
